e7bf7bfe8f912ae6ee91ef8f83158d62f195c2f7
ray <rpasko@mintel.com>
2018-10-31 15:55:22 -0500

    WIP - Third phase. Integrating bus into call center / tests.

--- call_center.py
+++ call_center.py
@@ -9,7 +9,9 @@ class CallCenter:
         self.alert_logs = []
         self.blacklist = set(blacklist)
 
-    def call(self, phone_number):
+    def call(self, event):
+        name, phone_number = event
+        
         t = self.time()
         self.call_logs.append((t, phone_number))
         if phone_number in self.blacklist:
@@ -26,20 +28,12 @@ def get_area_code(phone_number):
     return phone_number.split('/')[0]
 
 class CallAgency(CallCenter):
-    def __init__(self, call_centers, blacklist=[], *, time_adaptor=None):
-        super().__init__(blacklist, time_adaptor=time_adaptor)
-        self.call_centers = {}
-        for call_center in call_centers:
-            self.call_centers[call_center.area_code] = call_center
+    pass
 
-    def call(self, phone_number):
-        super().call(phone_number)
-
-        area_code = get_area_code(phone_number)
-        call_center = self.call_centers[area_code]
-        call_center.call(phone_number)
 
 class LocalCallCenter(CallCenter):
     def __init__(self, area_code, blacklist=[], *, time_adaptor=None):
         super().__init__(blacklist, time_adaptor=time_adaptor)
         self.area_code = area_code
+
+
--- tests/test_bus.py
+++ tests/test_bus.py
@@ -9,7 +9,7 @@ def test_a_receiver_gets_notified():
     events_received = []
     r1 = lambda event: events_received.append(event)
     bus = Bus()
-    bus.add_listener(r1)
+    bus.add_listener("Something", r1)
     event = ("Something", {"some": "thing"})
     bus.emit(event)
     assert events_received == [event]
@@ -20,8 +20,8 @@ def test_multiple_receivers_get_notified():
     r1 = lambda event: events_received.append(("r1", event))
     r2 = lambda event: events_received.append(("r2", event))
     bus = Bus()
-    bus.add_listener(r1)
-    bus.add_listener(r2)
+    bus.add_listener("Something", r1)
+    bus.add_listener("Something", r2)
     event = ("Something", {"some": "thing"})
     bus.emit(event)
     assert events_received == [("r1", event), ("r2", event)]
